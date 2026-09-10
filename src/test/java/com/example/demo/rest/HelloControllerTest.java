8bb1f564846aa6e4c486fa6b54fd175a27bef9e7
bitmaster18 <bitmaster18122023@gmail.com>
2026-09-10 18:47:23 +0530

    Initial commit for CI/CD demo

---
+++ src/test/java/com/example/demo/rest/HelloControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.rest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+	@Test
+    void hello_shouldReturnExpectedMessage() {
+        HelloController controller = new HelloController();
+        String response = controller.hello();
+        assertEquals("Hello from CI/CD!", response);
+    }
+
+}
